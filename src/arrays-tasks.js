965029df0a244c8a70acff84e93b3bd8bb82a83a
Aidana K <aidana.amnbk@gmail.com>
2025-01-03 16:16:27 +0500

    feat: solution for doubleArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -217,8 +217,11 @@ function getTail(arr, n) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  return arr.concat(arr);
 }
 
 /**
